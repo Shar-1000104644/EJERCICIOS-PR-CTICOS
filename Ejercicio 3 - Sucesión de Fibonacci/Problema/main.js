c91dafb5e972777b0975ae39085093c31ca0b01d
Rosales Calle,Rosario <1000104644.rr@gmail.com>
2016-06-24 15:58:18 -0500

    Uff! Terminé sucesión de Fibonacci :c muy tarde!  ...  y también el ejercicio número cinco AHORRO pero con ayuda de profe ;c

--- Ejercicio 3 - Sucesión de Fibonacci/Problema/main.js
+++ Ejercicio 3 - Sucesión de Fibonacci/Problema/main.js
@@ -1,43 +1,58 @@
 //Escribe aquí tú código
 
-// function calcular(){
-// 	var numero = document.getElementById('dato').value;
-// 	var sucesion = [];
-// 	var resultado = 1;
-
-// 	if(!isNaN(numero)){
-// 		for(var i=0 ; i=numero; i++){
-// 			sucesion.push(numero);
-// 			resultado +=sucesion[i]; 
-// 			function escribir();
-// 		}
-// 	}else{
-// 		alert('Ingresa un número');
-// 	}
-// }
-
-// function escribir(){
-// 	var letra= document.createElement('p');
-// 	var l = document.createTextNode('La sucesión es:' + sucesion.length);
-// 	letra.appendChild(l);
-// 	document.body.appendChild(letra);
-// }
-
 function calcular(){
-    var numero=parseInt(document.getElementById('dato').value);
     var var1 = 0;
     var var2 = 1;
     var var3;
+    var numero=parseInt(document.getElementById('dato').value);
+    document.getElementById('dato').value='';
 
+    mostrar('A','a',var1);
+    mostrar('B','c',var2);
+
+
+    // var A = document.createElement('p');
+    // var a=document.createTextNode(var1);
+    // A.appendChild(a);
+    // document.body.appendChild(A);
+
+    // var B = document.createElement('p');
+    // var b=document.createTextNode(var2);
+    // B.appendChild(b);
+    // document.body.appendChild(B);
 
     for(var i=3; i<=numero; i++)
     {
         var3 = var1 + var2;
         var1 = var2;    
         var2 = var3;
-    var letra = document.createElement('p');
-    letra.innerHTML(var1 + ' ');
-    letra.innerHTML(var2 + ' ');
-    letra.innerHTML(var3 + ' ');
+
+    mostrar('C','c',var3);
+
+    // var C = document.createElement('p');
+    // var c=document.createTextNode(var3);
+    // C.appendChild(c);
+    // document.body.appendChild(C); 
+
     }    
 }
+
+/*Esta función reemplaza a todo lo que esta en comentario, porque se repite mucho :c  y me daba flojera escribir */
+
+function mostrar(_elemento,_texto,_variable){
+    var _elemento = document.createElement('p');
+    var _texto=document.createTextNode(_variable);
+    _elemento.appendChild(_texto);
+    document.body.appendChild(_elemento);
+
+}
+
+
+// var a=0; var b=1; var c; var i; var x; 
+// x = prompt("Cuantos numeros quieres que te muestre?");
+// document.write(a+" "); 
+// document.write(b+" "); 
+// for(i=3;i<=x;i++) 
+// { 
+// c=a+b; a=b; b=c; 
+// document.write(c+" ");}
--- Ejercicio 5 - Ahorro/Problema/main.js
+++ Ejercicio 5 - Ahorro/Problema/main.js
@@ -1,54 +1,29 @@
 //Escribe aquí tú código
 
-// var mesesLista = ['enero','febrero','marzo'];
-// var mesesLista = new Array(3);
+var meses = [];
 
-// function wao(){
-// var dinero = document.getElementById('cantidad').value;
-// dinero=parseInt(dinero);
-// var mes = document.getElementById('mes').value;
-// 	for(var i=0; i>mesesLista.length; i++){
-// 		if(mes===0){
-// 			mesesLista[0][i]=dinero;
-// 			mesesLista.push(dinero);
-// 			document.getElementById('cantidad').value='';
-// 			document.getElementById('mes').value='';
-// 			alert(mesesLista[0][i]);
-// 		}
-// 		if(mes===1){
-// 			mesesLista[1][i]=dinero;
-// 			mesesLista.push(dinero);
-// 			document.getElementById('cantidad').value='';
-// 			document.getElementById('mes').value='';
-// 			alert(mesesLista[1][i]);
-// 		}
-// 		if(mes===2){
-// 			mesesLista[2][i]=dinero;
-// 			mesesLista.push(dinero);
-// 			document.getElementById('cantidad').value='';
-// 			document.getElementById('mes').value='';
-// 			alert(mesesLista[2][i]);
-// 		}	
-// 	}
-// }
+function iniciarAplicacion(){
+	var dinero = document.getElementById('monto');
 
 
-var mesesLista = new Array();
-mesesLista[0]=1,2,3,4,5;
-mesesLista[1]=6,7,8,9,10;
-mesesLista[2]=11,12,13,14,15;
-mesesLista[3]=16,17,18,19,20;
+	var m =['enero','febrero', 'marzo','abril','mayo','junio','julio','agosto','setiembre','octubre','noviembre','diciembre'];
+	meses['enero']=0;
+	meses['febrero']=0;
+	meses['marzo']=0;
+	meses['abril']=0;
+	meses['mayo']=0;
+	meses['junio']=0;
+	meses['julio']=0;
+	meses['agosto']=0;
+	meses['setiembre']=0;
+	meses['octubre']=0;
+	meses['noviembre']=0;
+	meses['diciembre']=0;
 
-mesesLista[0][0]=10;
-mesesLista[0][1]=20;
-mesesLista[0][2]=30;
+	for(var i=0; i<m.length; i++)
+	{
+		if(){
 
-mesesLista[1][0]=40;
-mesesLista[1][1]=50;
-mesesLista[1][2]=60;
-
-mesesLista[2][0]=70;
-mesesLista[2][1]=80;
-mesesLista[2][2]=90;
-
-alert(mesesLista[0][3]);
+		}
+	}
+ }
