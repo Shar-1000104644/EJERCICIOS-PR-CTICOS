b932114c9a174cf6b30fd1c38b3bf351e1e315e9
Rosales Calle,Rosario <1000104644.rr@gmail.com>
2016-07-04 18:29:01 -0500

    Oh

--- Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
+++ Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
@@ -1,1 +1,24 @@
 //Escribe aquí tú código
+var empleado={
+	nombre:'Gabriel',
+	ventas:0
+};
+
+var tienda = {
+	nombre: 'AQP1',
+	empleados: [],
+	cuidad: 'Arequipa',
+	ventas: 0,
+	sumarVentas:function(){
+		this.ventas=0;
+		for( var i = 0; i<this.empleados.length; i++){
+			this.ventas+=this.empleados[i].ventas;
+		}
+		return this.ventas;
+	}
+};
+
+tienda.empleados.push({nombre:'Emir', ventas:100});
+tienda.empleados.push({nombre:'Ale', ventas:200});
+tienda.empleados.push({nombre:'Brenda', ventas:300});
+tienda.empleados.push({nombre:'Daniel', ventas:400});
